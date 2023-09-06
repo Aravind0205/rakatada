f9d85b11ef2d0eacf313b5aa2324841e392df108
Aravind0205 <tmaravind5@gmail.com>
2023-09-06 12:53:37 +0100

    login signup and looking for blood design

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bdef524faebfd759"; }
+/******/ 		__webpack_require__.h = function() { return "c8d2179f67c6029b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/lookingForBlood.js
+++ pages/lookingForBlood.js
@@ -5,45 +5,47 @@ const LookingForBlood = () => {
         <div className="container mt-5">
            <div>Blood Stock Availability</div>
            <div className="row mt-5">
+               <div className="col-2">
+                   <select className="form-select form-select-md mb-3" aria-label=".form-select-lg example">
+                       <option defaultValue={""}>Select State</option>
+                       <option value="1">One</option>
+                       <option value="2">Two</option>
+                       <option value="3">Three</option>
+                   </select>
+               </div>
+               <div className="col-2">
+                   <select className="form-select form-select-md mb-3" aria-label=".form-select-lg example">
+                       <option defaultValue={""}>Select State</option>
+                       <option value="1">One</option>
+                       <option value="2">Two</option>
+                       <option value="3">Three</option>
+                   </select>
+               </div>
                <div className="col-3">
                    <select className="form-select form-select-md mb-3" aria-label=".form-select-lg example">
-                       <option defaultValue={"State"}>Select State</option>
-                       <option value="1">One</option>
-                       <option value="2">Two</option>
-                       <option value="3">Three</option>
+                       <option defaultValue={""}>Select Blood Group</option>
+                       <option value="A%2B">A+</option>
+                       <option value="A%2D">A-</option>
+                       <option value="B%2B">B+</option>
+                       <option value="B%2D">B-</option>
+                       <option value="AB%2B">AB+</option>
+                       <option value="AB%2D">AB-</option>
+                       <option value="O%2B">O+</option>
+                       <option value="O%2D">O-</option>
                    </select>
                </div>
                <div className="col-3">
                    <select className="form-select form-select-md mb-3" aria-label=".form-select-lg example">
-                       <option defaultValue={"State"}>Select State</option>
-                       <option value="1">One</option>
-                       <option value="2">Two</option>
-                       <option value="3">Three</option>
+                       <option defaultValue={""}>Select Blood Component</option>
+                       <option value="Whole Blood">Whole Blood</option>
+                       <option value=">Single Donor Platelet">Single Donor Platelet</option>
+                       <option value="Single Donor Plasma">Single Donor Plasma</option>
+                       <option value="Plasma">Plasma</option>
+                       <option value="Platelets">Platelets</option>
                    </select>
                </div>
-               <div className="col-2">
-                   <select className="form-select form-select-md mb-3" aria-label=".form-select-lg example">
-                       <option defaultValue={"State"}>Select State</option>
-                       <option value="1">One</option>
-                       <option value="2">Two</option>
-                       <option value="3">Three</option>
-                   </select>
-               </div>
-               <div className="col-2">
-                   <select className="form-select form-select-md mb-3" aria-label=".form-select-lg example">
-                       <option defaultValue={"State"}>Select State</option>
-                       <option value="1">One</option>
-                       <option value="2">Two</option>
-                       <option value="3">Three</option>
-                   </select>
-               </div>
-               <div className="col-2">
-                   <select className="form-select form-select-md mb-3" aria-label=".form-select-lg example">
-                       <option defaultValue={"State"}>Select State</option>
-                       <option value="1">One</option>
-                       <option value="2">Two</option>
-                       <option value="3">Three</option>
-                   </select>
+               <div className="col-2 mt-0">
+                   <button className="button-theme py-2 px-5">Search</button>
                </div>
            </div>
         </div>
